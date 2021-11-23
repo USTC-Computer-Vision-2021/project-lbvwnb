625122c9929f7cd79dc95b9ec5c4d030dac2b249
Aiddden <94847760+Aiddden@users.noreply.github.com>
2021-11-23 16:04:52 +0800

    Delete download_data_weights.sh

--- download_data_weights.sh
+++
@@ -1,8 +0,0 @@
-
-wget --no-check-certificate https://filebox.ece.vt.edu/~chengao/FGVC/data.zip
-unzip data.zip
-rm data.zip
-
-wget --no-check-certificate https://filebox.ece.vt.edu/~chengao/FGVC/weight.zip
-unzip weight.zip
-rm weight.zip
